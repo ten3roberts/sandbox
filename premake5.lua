77f5912a040fa5e37e705ad6bad1f3188b773efd
timmer <ten3roberts@gmail.com>
2020-04-26 21:19:58 +0200

    Cleaned up premake file

--- premake5.lua
+++ premake5.lua
@@ -1,19 +1,17 @@
 workspace "sandbox"
-	configurations "x64"
 
 -- Includes crescent from source
 include ("crescent")
-
-
 
 project "sandbox"
 	kind		"ConsoleApp"
 	language	"C"
 	targetdir	"bin"
 	objdir		"obj"
-	
+
 	-- Add C and H files
 	files { "src/**.c", "src/**.h" }
+
 	-- Add shaders to compilation
 	files { "assets/shaders/**.vert", "assets/shaders/**.frag" }	
 
