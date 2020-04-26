0d029605fe2faa038fa05ad913701ad74b8c5ac0
timmer <ten3roberts@gmail.com>
2020-04-26 20:17:23 +0200

    Added shaders to compilation

--- premake5.lua
+++ premake5.lua
@@ -4,14 +4,19 @@ workspace "sandbox"
 -- Includes crescent from source
 include ("crescent")
 
+
+
 project "sandbox"
 	kind		"ConsoleApp"
 	language	"C"
 	targetdir	"bin"
 	objdir		"obj"
 	
+	-- Add C and H files
 	files { "src/**.c", "src/**.h" }
-	
+	-- Add shaders to compilation
+	files { "assets/shaders/**.vert", "assets/shaders/**.frag" }	
+
 	-- Link crescent and GLFW
 	-- Vulkan is linked by crescent
 	links { "crescent", "GLFW" }
